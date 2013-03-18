a7427eea26e6209d8563b1f3a19a16e17cb01d1a
Christophe Coenraets <ccoenraets@gmail.com>
2013-03-18 13:50:38 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,65 @@
+window.addEventListener('load', function () {
+    new FastClick(document.body);
+}, false);
+
+// The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
+var homePage =
+    '<div>' +
+        '<div class="header"><h1>Page Slider</h1></div>' +
+        '<div class="scroller">' +
+            '<ul class="list">' +
+                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
+                '<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
+                '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
+            '</ul>' +
+        '</div>' +
+    '</div>';
+
+var detailsPage =
+    '<div>' +
+        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="scroller">' +
+            '<div class="robot">' +
+                '<img src="images/{{img}}"/>' +
+                '<h2>{{name}}</h2>' +
+                '<p>{{description}}</p>' +
+            '</div>' +
+        '</div>' +
+    '</div>';
+
+
+var slider = new PageSlider($("#container"));
+$(window).on('hashchange', route);
+
+// Basic page routing
+function route(event) {
+    var page,
+        hash = window.location.hash;
+
+    if (hash === "#page1") {
+        page = merge(detailsPage, {img: "buildbot.jpg", name: "Build Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+//        slider.slide($(page), "right");
+    } else if (hash === "#page2") {
+        page = merge(detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+//        slider.slide($(page), "right");
+    } else if (hash === "#page3") {
+        page = merge(detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+//        slider.slide($(page), "right");
+    }
+    else {
+        page = homePage;
+//        slider.slide($(homePage), "left");
+    }
+
+    slider.slidePage($(page));
+
+}
+
+// Primitive template processing. In a real-life app, use Handlerbar.js, Mustache.js or another template engine
+function merge(tpl, data) {
+    return tpl.replace("{{img}}", data.img)
+              .replace("{{name}}", data.name)
+              .replace("{{description}}", data.description);
+}
+
+route();
---
+++ pageslider.js
@@ -0,0 +1,62 @@
+/* Notes:
+ * - History management is currently done using window.location.hash.  This could easily be changed to use Push State instead.
+ * - jQuery dependency for now. This could also be easily removed.
+ */
+
+function PageSlider(container) {
+
+    var container = container,
+        currentPage;
+        stateHistory = [];
+
+    // Use this function if you want PageSlider to automatically determine the sliding direction based on the state history
+    this.slidePage = function(page) {
+
+        var l = stateHistory.length,
+            state = window.location.hash;
+
+        if (l === 0) {
+            stateHistory.push(state);
+            this.slidePageFrom(page);
+            return;
+        }
+        if (state === stateHistory[l-2]) {
+            stateHistory.pop();
+            this.slidePageFrom(page, 'left');
+        } else {
+            stateHistory.push(state);
+            this.slidePageFrom(page, 'right');
+        }
+
+    }
+
+    // Use this function directly if you want to control the sliding direction outside PageSlider
+    this.slidePageFrom = function(page, from) {
+
+        container.append(page);
+
+        if (!currentPage || !from) {
+            page.attr("class", "page center");
+            currentPage = page;
+            return;
+        }
+
+        // Position the page at the starting position of the animation
+        page.attr("class", "page " + from);
+
+        if (currentPage) {
+            currentPage.one('webkitTransitionEnd', function(e) {
+                e.target.remove();
+            });
+        }
+
+        // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
+        container[0].offsetWidth;
+
+        // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
+        page.attr("class", "page transition center");
+        currentPage.attr("class", "page transition " + (from === "left" ? "right" : "left"));
+        currentPage = page;
+    }
+
+}
